4f8167e2f7a7fff3da969ed94ab6df4b027d839c
jitendrakumarh7 <120735188+jitendrakumarh7@users.noreply.github.com>
2023-05-28 10:56:36 +0530

    n=int(input()) l=list(map(int,input().split())) mx=max(l) s=0 c=0 ct=0 case=0 for i in range(1,1000):     c=0     for j in l:         if i==j:             c=c+1     if c==i:         s=s+i         case=1         ct=ct+1 if case==1:     print(format(s/ct,"0.2f")) else:     print("-1")

---
+++ Average_of_super_elements.py
@@ -0,0 +1,21 @@
+n=int(input())
+l=list(map(int,input().split()))
+mx=max(l)
+s=0
+c=0
+ct=0
+case=0
+for i in range(1,1000):
+    c=0
+    for j in l:
+        if i==j:
+            c=c+1
+    if c==i:
+        s=s+i
+        case=1
+        ct=ct+1
+if case==1:
+    print(format(s/ct,"0.2f"))
+else:
+    print("-1")
+        
